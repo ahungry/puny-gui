60ce703600993a43cc329135d8e2c7427d2de055
Matthew Carter <m@ahungry.com>
2020-04-24 01:55:17 -0400

    update build scripts to include demo of dog

--- package-linux.sh
+++ package-linux.sh
@@ -10,5 +10,7 @@ cp ./*.janet $dir/
 cp ./*.bin $dir/
 cp ./logo.png $dir/
 cp ./logo2.png $dir/
+cp -R ./lib $dir/
+cp -R ./examples $dir/
 
 tar czvf $dir.tar.gz $dir
--- package-windows.sh
+++ package-windows.sh
@@ -6,5 +6,7 @@ zip -r super-janet-app-win64.zip \
     ./*.janet \
     ./*.dll \
     ./*.exe \
+    ./lib \
+    ./examples \
     ./logo.png \
     ./logo2.png
